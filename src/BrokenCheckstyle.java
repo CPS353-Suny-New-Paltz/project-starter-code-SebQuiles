f1a0ec3f5535f4d6f7be33af441f04f19de9072a
Sebastian Quiles <squiles181@gmail.com>
2025-09-03 22:46:14 -0400

    Add LineLength Checkstyle rule and test files for verification
    Added LineLength Checkstyle rule and test files for Checkpoint 1 verification.
    This will trigger Gradle and Checkstyle checks to confirm branch protection is working.
    
    Co-Authored-By: kbrainard1 <47262866+kbrainard1@users.noreply.github.com>

---
+++ src/BrokenCheckstyle.java
@@ -0,0 +1,5 @@
+public class BrokenCheckstyle {
+    public static void main(String[] args) {
+        System.out.println("This line is way toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long!");
+    }
+}
---
+++ src/TestGradle.java
@@ -0,0 +1,6 @@
+public class TestGradle {
+	    public static void main(String[] args) {
+	        System.out.println("This is broken code"
+	    }
+	}
+
