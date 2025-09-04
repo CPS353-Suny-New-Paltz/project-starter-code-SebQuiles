5d63907480006f573fa8edccbbc824700136e09c
Sebastian Quiles <squiles181@gmail.com>
2025-09-03 23:17:17 -0400

    Update BrokenCheckstyle.java

--- src/BrokenCheckstyle.java
+++ src/BrokenCheckstyle.java
@@ -2,4 +2,4 @@ public class BrokenCheckstyle {
     public static void main(String[] args) {
         System.out.println("This line is way toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long!");
     }
-} //test
+} //test test
