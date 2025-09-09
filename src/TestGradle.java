fd0fe29d5ef74e61785faf0d3bf8fa026988d6d5
Sebastian Quiles <squiles181@gmail.com>
2025-09-09 00:34:21 -0400

    Fix BrokenCheckstyle.java to satisfy Checkstyle rule
    Co-Authored-By: kbrainard1 <47262866+kbrainard1@users.noreply.github.com>

--- src/TestGradle.java
+++ src/TestGradle.java
@@ -1,5 +1,5 @@
 public class TestGradle {
 	public static void main(String[] args) {
-	        System.out.println("This is broken code" // test test
+	        System.out.println("This is broken code"); // test test
 	    }
 }
