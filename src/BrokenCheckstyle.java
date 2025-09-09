2527dbb2307ce436fb8ff359e196d9c2df4c1368
Sebastian Quiles <squiles181@gmail.com>
2025-09-09 00:27:47 -0400

    Update BrokenCheckstyle.java
    Co-Authored-By: kbrainard1 <47262866+kbrainard1@users.noreply.github.com>

--- src/BrokenCheckstyle.java
+++ src/BrokenCheckstyle.java
@@ -1,5 +1,5 @@
 public class BrokenCheckstyle {
     public static void main(String[] args) {
-        System.out.println("This line is way toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long!");
+        System.out.println("This line is intentionally wayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy too long to trigger the LineLength rule!");
     }
-} //test test 
+}
