b8cf2411233d1864947353ac0b08ea477dfa1ef3
Sebastian Quiles <squiles181@gmail.com>
2025-10-28 19:19:21 -0400

    update prototypes

--- src/project/annotations/NetworkAPIPrototype.java
+++ src/project/annotations/NetworkAPIPrototype.java
@@ -1,4 +1,12 @@
 package project.annotations;
 
-public interface NetworkAPIPrototype{
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NetworkAPIPrototype {
+	// Marker annotation, should be applied to a method within a prototype class
 }
--- src/project/annotations/ProcessAPIPrototype.java
+++ src/project/annotations/ProcessAPIPrototype.java
@@ -1,5 +1,13 @@
 package project.annotations;
 
-public interface ProcessAPIPrototype {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ProcessAPIPrototype {
 	// Marker annotation, should be applied to a method within a prototype class
 }
