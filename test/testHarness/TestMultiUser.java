45b6ad1ec15ba82da38acbf50db65604071d3e9a
Sebastian Quiles <squiles181@gmail.com>
2025-12-15 23:46:24 -0500

    Fix variable name in loadAllOutput method for consistency

--- test/testHarness/TestMultiUser.java
+++ test/testHarness/TestMultiUser.java
@@ -90,7 +90,7 @@ public class TestMultiUser {
 		Assertions.assertEquals(singleThreaded, multiThreaded);
 	}
 
-	private List<String> loadAllOutput(String prefix, int nThreads) throws IOException {
+	private List<String> loadAllOutput(String prefix, int numThreads) throws IOException {
 		List<String> result = new ArrayList<>();
 		for (int i = 0; i < nThreads; i++) {
 			File multiThreadedOut = 
