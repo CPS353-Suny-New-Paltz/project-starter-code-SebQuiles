3e9bd8ab55ac9eff8db0822796130a957023b698
Sebastian Quiles <squiles181@gmail.com>
2025-12-15 23:42:26 -0500

    Fix variable name consistency in TestMultiUser class for thread count

--- test/testHarness/TestMultiUser.java
+++ test/testHarness/TestMultiUser.java
@@ -47,9 +47,9 @@ public class TestMultiUser {
     }
 	@Test
 	public void compareMultiAndSingleThreaded() throws Exception {
-		int nThreads = 4;
+		int numThreads = 4;
 		List<TestUser> testUsers = new ArrayList<>();
-		for (int i = 0; i < nThreads; i++) {
+		for (int i = 0; i < numThreads; i++) {
 			testUsers.add(new TestUser(coordinator));
 		}
 		
@@ -58,7 +58,7 @@ public class TestMultiUser {
 		StorageEngineAPIImpl storage = new StorageEngineAPIImpl();
 		NumberToWordsAPIImpl converter = new NumberToWordsAPIImpl();
 		UserComputeAPI singleThreadApi = new UserComputeAPIImpl(storage, converter);
-		for (int i = 0; i < nThreads; i++) {
+		for (int i = 0; i < numThreads; i++) {
 			File singleThreadedOut = 
 					new File(singleThreadFilePrefix + i);
 			singleThreadedOut.deleteOnExit();
@@ -70,7 +70,7 @@ public class TestMultiUser {
 		ExecutorService threadPool = Executors.newCachedThreadPool();
 		List<Future<?>> results = new ArrayList<>();
 		String multiThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.multiThreadOut.tmp";
-		for (int i = 0; i < nThreads; i++) {
+		for (int i = 0; i < numThreads; i++) {
 			File multiThreadedOut = 
 					new File(multiThreadFilePrefix + i);
 			multiThreadedOut.deleteOnExit();
@@ -85,8 +85,8 @@ public class TestMultiUser {
 		threadPool.shutdownNow();
 		
 		// Check that the output is the same for multi-threaded and single-threaded
-		List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix, nThreads);
-		List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, nThreads);
+		List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix, numThreads);
+		List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, numThreads);
 		Assertions.assertEquals(singleThreaded, multiThreaded);
 	}
 
